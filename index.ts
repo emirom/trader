e8346ff5a4077cf41ba2eba857dc5fc1d581e6de
amir meyari <forsite73@gmail.com>
2020-10-03 21:40:15 +0330

    url added

--- index.ts
+++ index.ts
@@ -12,10 +12,24 @@ import fetch from "node-fetch";
 //   });
 
 // const url = "http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=55254206302462116"
-const url = "http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=55254206302462116"
+const id = 55254206302462116
+// const daily = `http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=${id}`
+const lastThirtyDays = `http://www.tsetmc.com/loader.aspx?ParTree=151311&i=${id}#`
 
-
-fetch(url)
+fetch(lastThirtyDays)
+fetch(`http://members.tsetmc.com/tsev2/data/InstTradeHistory.aspx?i=${id}&Top=999999&A=0`, {
+  "headers": {
+    "accept": "text/plain, */*; q=0.01",
+    "accept-language": "en-US,en;q=0.9",
+    "proxy-connection": "keep-alive"
+  },
+  "referrer": "http://www.tsetmc.com/loader.aspx?ParTree=151311&i=55254206302462116",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "omit"
+})
   .then((res) => res.text())
   .then((text) => console.log(text));
 
