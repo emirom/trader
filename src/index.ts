1c61fdf8123d06edda06e283f8f133ff7f71f30d
amir meyari <forsite73@gmail.com>
2020-10-04 12:09:20 +0330

    tsetmc added to get  assets

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,42 @@
 // import cheerio from "cheerio";
 // import axios from "axios";
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
+import { assets as getAsset } from 'tsetmc-api'
 
-// axios
-//   .get("https://www.seo.ir/")
-//   .then((res) => {
-//     console.log("res", res);
-//   })
-//   .catch((err) => {
-//     console.log("err", err);
-//   });
 
 // const url = "http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=55254206302462116"
-const id = 55254206302462116
-// const daily = `http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=${id}`
-const lastThirtyDays = `http://www.tsetmc.com/loader.aspx?ParTree=151311&i=${id}#`
+// const id = 55254206302462116
+// // const daily = `http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=${id}`
+// const lastThirtyDays = `http://www.tsetmc.com/loader.aspx?ParTree=151311&i=${id}#`
 
-fetch(lastThirtyDays)
-fetch(`http://members.tsetmc.com/tsev2/data/InstTradeHistory.aspx?i=${id}&Top=999999&A=0`, {
-  "headers": {
-    "accept": "text/plain, */*; q=0.01",
-    "accept-language": "en-US,en;q=0.9",
-    "proxy-connection": "keep-alive"
-  },
-  "referrer": "http://www.tsetmc.com/loader.aspx?ParTree=151311&i=55254206302462116",
-  "referrerPolicy": "no-referrer-when-downgrade",
-  "body": null,
-  "method": "GET",
-  "mode": "cors",
-  "credentials": "omit"
-})
-  .then((res) => res.text())
-  .then((text) => console.log(text));
+// fetch(lastThirtyDays)
+//   .then((res) => res.text())
+//   .then((result) => console.log(result))
+//   .catch(err => console.error(err, "err"))
 
-//  ts-node index.ts
+// let res
+// try {
+//   res = await assets()
+// }
+// catch (err) {
+//   console.log("assets err", err)
+// }
+
+
+// const getAssets = () => {
+//   var assetsList
+getAsset()
+  .then(res => { console.log(res) })
+  .catch(err => console.log("assets err", err))
+
+  // setTimeout(() =>
+  //   console.log(assetsList)
+  //   , 3000);
+  // return assetsList
+// }
+
+
+// const assets = getAssets()
+// console.log(assets)
+// const assetIds = assets && assets.map(asset => asset.id)
+// console.log(assetIds)
