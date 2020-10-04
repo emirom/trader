8e072bf32299e69ff3b7718afa206abc0fa44562
amir meyari <forsite73@gmail.com>
2020-10-04 12:38:58 +0330

    with try catch and func revised

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,35 @@
 // import fetch from "node-fetch";
 import { assets as getAsset } from 'tsetmc-api'
 
-
-// const url = "http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=55254206302462116"
-// const id = 55254206302462116
-// // const daily = `http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i=${id}`
-// const lastThirtyDays = `http://www.tsetmc.com/loader.aspx?ParTree=151311&i=${id}#`
-
-// fetch(lastThirtyDays)
-//   .then((res) => res.text())
-//   .then((result) => console.log(result))
-//   .catch(err => console.error(err, "err"))
-
-// let res
-// try {
-//   res = await assets()
-// }
-// catch (err) {
-//   console.log("assets err", err)
-// }
-
-
-// const getAssets = () => {
-//   var assetsList
+//logs correctly
 getAsset()
   .then(res => { console.log(res) })
   .catch(err => console.log("assets err", err))
 
-  // setTimeout(() =>
-  //   console.log(assetsList)
-  //   , 3000);
-  // return assetsList
-// }
+
+// a func with try catch
+const withTry = async () => {
+  let res
+  try {
+    res = await getAsset()
+  }
+  catch (err) {
+    console.log("assets err", err)
+  }
+  return res
+}
+
+const assests = await withTry()
+console.log(assests)
 
 
-// const assets = getAssets()
-// console.log(assets)
-// const assetIds = assets && assets.map(asset => asset.id)
-// console.log(assetIds)
+//wit func
+const getAssets = () => {
+  let assetsList
+  getAsset()
+    .then(res => { assetsList = res })
+    .catch(err => console.log("assets err", err))
+  return assetsList
+}
+const assets = getAssets()
+console.log(assets)
