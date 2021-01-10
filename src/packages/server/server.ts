0bbead0253684b457f03be9f6a1902f46f7cd885
amir meyari <forsite73@gmail.com>
2021-01-10 20:10:47 +0330

    run app after connection like teamChe

--- src/packages/server/server.ts
+++ src/packages/server/server.ts
@@ -1,42 +1,47 @@
-import mongoose, { ConnectOptions } from "mongoose";
 import express from "express";
+import mongoose, { ConnectOptions } from "mongoose";
 
-export const startServer = async () => {
-  const app = express();
-  app.on("ready", (app) => onReady(app));
-
-  await mongoose.connect(process.env.DB_URI, expressOptions);
-  const mongo = mongoose.connection;
-  mongo.on("error", (err) => onError(err));
-  mongo.on("connected", OnConnection);
-  mongo.once("open", (app) => app.emit("ready"));
-
-  return mongo;
-};
-//-------------events :
-
-const OnConnection = () => {
-  console.log("established initial mongo connection !");
-};
-
-/**
- * waiting for db connections before app listen
- * @link https://blog.cloudboost.io/waiting-for-db-connections-before-app-listen-in-node-f568af8b9ec9
- * @param app
- */
-const onReady = (app) => {
-  app.listen(process.env.APP_PORT, () =>
-    console.log("app is ready and listening on port:")
-  );
-};
-
-const onError = (err) => {
-  console.error.bind(console, "connection error :", err);
-  // logger.error('connection to mongo failed ' + err);
-};
-
-const expressOptions: ConnectOptions = {
+const mongooseOptions: ConnectOptions = {
   useNewUrlParser: true, // omit depricated Uri
   useUnifiedTopology: true, // allow new document
   useCreateIndex: true,
+};
+
+export const startServer = async () => {
+  console.log("inside start server", process.env.DB_URI);
+
+  mongoose.connect(process.env.DB_URI, mongooseOptions);
+  const mongo = mongoose.connection;
+  mongo.on("error", (err) => onError(err));
+  // mongo.once("open", (app) => {
+  //   console.log("connection ready", app);
+  //   app.emit("ready");
+  // });
+  // app.on("ready", (app) => onReady(app));
+  const app = express();
+  // app.use(cors());;
+  // router(app);
+  app.listen(process.env.APP_PORT, () =>
+    console.log("app is ready and listening on port:", process.env.APP_PORT)
+  );
+  return mongo;
+};
+
+/**
+ * -------------events :
+ * waiting for db connections before app listen
+ * @link https://blog.cloudboost.io/waiting-for-db-connections-before-app-listen-in-node-f568af8b9ec9
+ */
+// / const OnConnection = () => {
+//   console.log("established initial mongo connection !");
+// };
+
+// const onReady = (app) => {
+//   app.listen(process.env.APP_PORT, () =>
+//     console.log("app is ready and listening on port:")
+//   );
+// };
+const onError = (err) => {
+  console.error.bind(console, "connection error :", err);
+  // logger.error('connection to mongo failed ' + err);
 };
