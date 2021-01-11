9eb3963968d49bea510aaadf9ff6c10a4809cb86
amir meyari <forsite73@gmail.com>
2021-01-11 13:54:41 +0330

    keep storage temp for tests

---
+++ src/packages/server/utils/tempStorage.ts
@@ -0,0 +1,5 @@
+// import { LocalStorage } from "node-localstorage";
+// const local = new LocalStorage("./scratch");
+
+// localStorage.setItem("data", response.data);
+// const rows = local.getItem("rows");
