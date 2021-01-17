db0b0d3f8a561ae344ca4e9d9f1903b7484f7a55
amir meyari <forsite73@gmail.com>
2021-01-18 00:13:23 +0330

    refined the "hajmMashkuk" filter

--- src/packages/server/filters/hajmMahskuk.ts
+++ src/packages/server/filters/hajmMahskuk.ts
@@ -1,14 +1,9 @@
-import { Ih } from "../models/dayHistory";
 import { ISymbol } from "../models/Symbol";
 
 /**
  * سهم‌هایی که امروز حداقل 5 برابر میانگین حجم ماهیانه معامله شده‌اند (حجم‌های مشکوک)
  * @param symbols
- * @param histories
  */
-export const hajmMashkuk = (symbols: ISymbol[], histories: Ih[]) => {
-  return symbols.filter((symbol) => {
-    const ih = histories.find((h) => h.inscode === symbol.inscode);
-    // return symbol.tvol > 5 * avg30_QTotTran5J(ih.history);
-  });
+export const hajmMashkuk = (symbols: ISymbol[]) => {
+  return symbols.filter((symbol) => symbol.tvol > 5 * symbol.avg30_QTotTran5J);
 };
