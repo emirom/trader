7b524bfefb7760bb0afe778e8e6fed72dc9c807a
amir meyari <forsite73@gmail.com>
2021-01-18 00:04:50 +0330

    addd got instead of axios

--- src/queries/getAll.ts
+++ src/queries/getAll.ts
@@ -1,10 +1,9 @@
-import axios from "axios";
-
+import got from "got";
 export const getAll = async () => {
-  const raw = await axios.get(
+  const raw = await got.get(
     "http://www.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0"
   );
-  const data: string = raw.data;
+  const data: string = raw.body;
   const list = data.split(";");
   const idList = list.map((item) => item.split(",")[0]);
   return idList[10];
