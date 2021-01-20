c25f5c32cdf047ae1cd630b3ba6170d2d4617474
amir meyari <forsite73@gmail.com>
2021-01-20 11:46:01 +0330

    renamed ct to customer type

--- src/packages/server/controllers/customerType/getCt.ts
+++
@@ -1,3 +0,0 @@
-const updateCt = () => {
-  const data = "";
-};
---
+++ src/packages/server/controllers/customerType/updateCustomerType.ts
@@ -0,0 +1,5 @@
+import { ISymbol } from "../../models/Symbol";
+
+export const updateCustomerType = async (symbols: ISymbol[]) => {
+  const ct = "";
+};
