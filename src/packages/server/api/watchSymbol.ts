ba8547a74fc79cbd3174d54df5e293b27a92323d
amir meyari <forsite73@gmail.com>
2021-01-20 11:23:36 +0330

    customer type added to watch symbol

---
+++ src/packages/server/api/watchSymbol.ts
@@ -0,0 +1,73 @@
+import got from "got/dist/source";
+import { newAliveAgent } from "../utils/got";
+import { mergeObjects } from "../utils/mergeObjects";
+
+export const watchSymbol = async (id: number) => {
+  const url = `http://www.tsetmc.com/tsev2/data/instinfodata.aspx?i=${id}&c=27`;
+  const response = await got.get(url, newAliveAgent());
+  const raw = response.body;
+  const parts = raw.split(";");
+  const prices = extractPrices(parts[0]);
+  const limits = extractLimits(parts[2]);
+  const customerType = extractCustomerType(parts[4]);
+
+  console.table(limits);
+  console.table(prices);
+  console.table(customerType);
+
+  return {
+    ...limits,
+    ...prices,
+    ...customerType,
+  };
+};
+
+export const watchCustomerType = async (id: number) => {
+  const url = `http://www.tsetmc.com/tsev2/data/instinfodata.aspx?i=${id}&c=27`;
+  const response = await got.get(url, newAliveAgent());
+  const raw = response.body;
+  const parts = raw.split(";");
+  // console.table(ct);
+  return extractCustomerType(parts[4]);
+};
+
+const extractLimits = (rawLimits) => {
+  const withEmpty = rawLimits.split(",");
+  const notEmptyRows = [withEmpty[0], withEmpty[1], withEmpty[2]];
+
+  const resultArray = notEmptyRows.map((row, index) => {
+    const columns = row.split("@");
+    // z : zahlen (integer number), p :price, q : quantity, d: dealer, o: owner
+    return {
+      ["zd" + (+index + 1)]: +columns[0],
+      ["qd" + (+index + 1)]: +columns[1],
+      ["pd" + (+index + 1)]: +columns[2],
+      ["po" + (+index + 1)]: +columns[3],
+      ["qo" + (+index + 1)]: +columns[4],
+      ["zo" + (+index + 1)]: +columns[5],
+    };
+  });
+  return mergeObjects(resultArray);
+};
+const extractCustomerType = (rawCt) => {
+  const columns = rawCt.split(",");
+  return {
+    Buy_I_Volume: +columns[0],
+    Buy_N_Volume: +columns[1],
+    Sell_I_Volume: +columns[3],
+    Sell_N_Volume: +columns[4],
+    Buy_CountI: +columns[5],
+    Buy_CountN: +columns[6],
+    Sell_CountI: +columns[8],
+    Sell_CountN: +columns[9],
+  };
+};
+
+const extractPrices = (rawPrices) => {
+  const columns = rawPrices.split(",");
+  return {
+    pl: +columns[3], // آخرین معامله
+    pc: +columns[4], // قیمت پایانی
+    pf: +columns[5], // اولین قیمت
+  };
+};
