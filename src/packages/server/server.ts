78cc7b088b9c9f3d164725003b4d53c958dfbd9e
amir meyari <forsite73@gmail.com>
2021-01-27 12:33:34 +0330

    resfactored server

--- src/packages/server/server.ts
+++ src/packages/server/server.ts
@@ -8,40 +8,34 @@ const mongooseOptions: ConnectOptions = {
 };
 
 export const startServer = async () => {
-  console.log("inside start server", process.env.DB_URI);
+  try {
+    mongoose.connect(process.env.DB_URI, mongooseOptions);
+    const mongo = mongoose.connection;
+    mongo.on("error", (err) => onError(err));
+    console.log("server started :", process.env.DB_URI);
 
-  mongoose.connect(process.env.DB_URI, mongooseOptions);
-  const mongo = mongoose.connection;
-  mongo.on("error", (err) => onError(err));
-  // mongo.once("open", (app) => {
-  //   console.log("connection ready", app);
-  //   app.emit("ready");
-  // });
-  // app.on("ready", (app) => onReady(app));
-  const app = express();
-  // app.use(cors());;
-  // router(app);
-  app.listen(process.env.APP_PORT, () =>
-    console.log("app is ready and listening on port:", process.env.APP_PORT)
-  );
-  return mongo;
+    const app = express();
+    // app.use(cors());;
+    // app.use(express.static(path.join(__dirname, 'public')));
+    ``;
+    app.listen(process.env.APP_PORT, () =>
+      console.log(
+        `app is ready and listening on http://localhost:${process.env.APP_PORT}`
+      )
+    );
+
+    app.get("/", (req, res) => {
+      console.log(req);
+      return res.send("Welcome to our amazing trader app!");
+    });
+
+    return { mongo, app };
+  } catch (error) {
+    console.log("\nError on start server: " + error);
+    return error;
+  }
 };
 
-/**
- * -------------events :
- * waiting for db connections before app listen
- * @link https://blog.cloudboost.io/waiting-for-db-connections-before-app-listen-in-node-f568af8b9ec9
- */
-// / const OnConnection = () => {
-//   console.log("established initial mongo connection !");
-// };
-
-// const onReady = (app) => {
-//   app.listen(process.env.APP_PORT, () =>
-//     console.log("app is ready and listening on port:")
-//   );
-// };
 const onError = (err) => {
   console.error.bind(console, "connection error :", err);
-  // logger.error('connection to mongo failed ' + err);
 };
