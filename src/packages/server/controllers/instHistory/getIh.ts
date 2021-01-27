64365a27d1e9a7aed1b1e8a19c92989b8d4e6ad2
amir meyari <forsite73@gmail.com>
2021-01-27 12:34:46 +0330

    renamed to dayHistory

--- src/packages/server/controllers/instHistory/getIh.ts
+++
@@ -1,4 +0,0 @@
-import Ih from "../../models/DayHistory";
-
-export const getIntraHistory = async (inscode: number) =>
-  await Ih.findOne({ inscode });
--- src/packages/server/controllers/instHistory/index.ts
+++
@@ -1,5 +0,0 @@
-import { updateIntraHistory } from "./updateIh";
-
-export const intraHistory = (app) => {
-  app.post("/api/ih/update", updateIntraHistory);
-};
